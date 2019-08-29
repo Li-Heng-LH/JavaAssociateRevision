ef3f78ca07b88a401ccdffc72ccce4335dc21e30
Li Heng <liheng6567@gmail.com>
2019-08-29 23:20:56 +0800

    toString : subsequent changes to sb do not affect string

--- src/StringBuilderTest.java
+++ src/StringBuilderTest.java
@@ -9,4 +9,14 @@ public class StringBuilderTest {
         //A new String object is allocated and initialized each time toString() is called
         System.out.println(s1 == s2);
     }
+
+    public void toStringTest2() {
+        StringBuilder sb = new StringBuilder("abc");
+        String str = sb.toString();
+        sb.append("defg");
+
+        //str should still be abc.
+        //Subsequent changes to this sequence do not affect the contents of the String.
+        System.out.println(str);
+    }
 }
