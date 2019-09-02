5bd83d2d26f237ffc76ec386a5dca6a0dade0e97
Li Heng <liheng6567@gmail.com>
2019-09-02 21:56:01 +0800

    DoubleInitialisationTest: cannot initialise with no value

---
+++ src/ArrayListTest.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ArrayListTest {
+
+    public static void initialisationTest() {
+        ArrayList<Double> arrayList = new ArrayList<>();
+
+        //empty ArrayList -> accessing index will get error
+
+    }
+
+}
---
+++ src/InitialisationTest.java
@@ -0,0 +1,13 @@
+public class InitialisationTest {
+
+    public static void DoubleInitialisationTest() {
+        //Not allowed to initialise with no value.
+        //Double d = new Double();
+
+        //unnecessary boxing
+        // Double d = new Double(0.0);
+
+        Double d = 0.0;
+        System.out.println(d);
+    }
+}
