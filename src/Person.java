f4ec8da767b415db174e5b371a4ad3fea0bacb56
Li Heng <liheng6567@gmail.com>
2019-09-03 22:01:49 +0800

    proper way to chain constructors: this()

---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person() {
+        //Wrong way to chain constructors
+        //Person("abc", 20);
+
+        //proper way to chain constructors
+        this("abc", 20);
+    }
+
+    public Person (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
