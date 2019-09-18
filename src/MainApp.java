323f3daa5be6ff42d48c390b7737eaff158938bb
Li Heng <liheng6567@gmail.com>
2019-09-18 23:38:40 +0800

    Main file with multiple classes

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,6 +4,7 @@
 //Need a separate import for each subpackage
 //import mainApp.orders.items.*;
 
+import java.sql.SQLOutput;
 import java.util.*;
 import java.time.LocalDate;
 
@@ -14,10 +15,20 @@ import mainApp.orders.StringTest;
 public class MainApp {
 
     public static void main(String[] args) {
-        StringTest.assignThenPrintTest();
+        Message.main(args);
     }
 
     private static void throwsException() throws Exception {
         System.out.println("All fine");
+    }
+}
+
+//Cannot have 2 public classes
+//public class Message { //error
+class Message {
+
+    //public static void main(String [] args) { //can run individually
+    static void main(String [] args) {
+        System.out.println("Message!");
     }
 }
