45aa67b3e670de6674921b5fdc810cabc663974c
Li Heng <liheng6567@gmail.com>
2019-12-05 23:25:48 +0800

    Add unreached statement after finally.

--- src/MainApp.java
+++ src/MainApp.java
@@ -17,6 +17,7 @@ public class MainApp {
 
     public static void main(String[] args) throws Exception {
         TestTryCatch.testFinally();
+        System.out.println("After finally...");
     }
 
     private static void throwsException() throws Exception {
