4cf25b6976a73d68b58cf27a4b098a2e514c6d3c
Li Heng <liheng6567@gmail.com>
2019-12-15 15:27:43 +0800

    java is pass by value

--- src/mainApp/ReferencePassingTest.java
+++ src/mainApp/ReferencePassingTest.java
@@ -5,15 +5,25 @@ public class ReferencePassingTest {
     public static void referenceIsCopied() {
         Message obj = new Message("Hello World");
         obj.print(); //"Hello World"
-        changeMsg(obj); //reference is COPIED into method
+
+        reassignMsg(obj); //reference is COPIED into method
         obj.print(); //Still the same reference as before, therefore, "Hello World"
+
+        changeMsg(obj); //reference is COPIED into method
+        obj.print(); //"Hello World 2"
     }
 
     private static void changeMsg(Message msg) {
         //reference obj is copied into msg
+        //both obj and msg are pointing to same Message
+        msg.message = "Hello World 2";
+    }
+
+    private static void reassignMsg(Message msg) {
+        //reference obj is copied into msg
         //msg is another reference, not the same as obj
+        //therefore, reassign does not affect obj
         msg = new Message("Bye bye");
-        System.out.println(msg.message); //"Bye bye"
     }
 }
 
