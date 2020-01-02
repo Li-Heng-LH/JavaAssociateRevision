19a8e3fe9f93330e3bb6c0630cef3d4fefb3c1ce
Li Heng <liheng6567@gmail.com>
2020-01-02 21:17:13 +0800

    Rules for overriding methods that throw exceptions

---
+++ src/mainApp/OverrideAndExceptionTest.java
@@ -0,0 +1,40 @@
+package mainApp;
+
+public class OverrideAndExceptionTest {}
+
+class Animal {
+
+    public void method () throws ParentException {}
+
+}
+
+class Dog extends Animal {
+
+    //Not throwing exception is allowed
+    //public void method() {} //Not throwing exception is allowed
+
+    //Throwing child exception is allowed
+    //public void method() throws ChildExceptionA {}
+
+    //Throwing multiple child exceptions is allowed
+    //public void method() throws ChildExceptionA, ChildExceptionB {}
+
+    //Throwing parent and child exceptions is allowed
+    public void method() throws ParentException, ChildExceptionA, ChildExceptionB {}
+
+
+
+    //Throwing more generic exception is not allowed
+    //public void method() throws Exception {}
+
+    //Throwing another exception is not allowed
+    //public void method() throws AnotherException {}
+}
+
+class ParentException extends Exception {}
+
+class ChildExceptionA extends ParentException {}
+
+class ChildExceptionB extends ParentException {}
+
+class AnotherException extends Exception {}
